6e765a70403d7eb515bc6c30ae91cb7bfc649e0e
Sung Bae <singsang2@gmail.com>
2020-03-19 14:00:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+katz_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(list)
+  if list.length == 0 
+    puts "The line is currently empty"
+  else
+    print "The line is currently: "
+    list.each_with_index do |n, i|
+      print "#{i+1}. #{n}"
+      if i != list.length-1
+        print " "
+      end
+    end
+    print "\n"
+  end
+end
+line(katz_deli)
+
+def now_serving(list)
+  if list.length != 0
+    puts "Currently serving #{list.shift()}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
+
+def take_a_number(list, name)
+  list.push(name)
+  puts "Welcome, #{name}. You are number #{list.length} in line."
+end
+
